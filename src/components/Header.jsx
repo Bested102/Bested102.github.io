def13ea3d660444ee78bc87724eb2d49a1a94260
Omar <omar4444yasser@gmail.com>
2024-08-31 19:00:02 +0300

    e

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,11 @@ export default function Header() {
         nav.current.classList.add("hide-mobile");
         menuItem.forEach((e) => (e.onclick = closeMobile));
         document.querySelector(".nav svg").onclick = closeMobile;
+        document.body.addEventListener("click", (e) => {
+          if (e.target === document.body && nav.current.classList.contains("show-mobile")){
+            closeMobile()
+          }
+        });
       } else {
         document.querySelector("#theme-toggle").before(nav.current);
         nav.current.classList.remove("show-mobile");
@@ -46,9 +51,7 @@ export default function Header() {
         >
           <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
         </svg>
-        <h1 className="logo">
-          Omar.dev
-        </h1>
+        <h1 className="logo">Omar.dev</h1>
         <ul className="nav" ref={nav}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
